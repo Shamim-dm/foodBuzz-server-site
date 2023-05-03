b90066e9556944bbce20322d8ff8e791c4bef699
Shamim-DM <mdshamimmiah000000@gmail.com>
2023-05-04 01:45:37 +0600

    add index

---
+++ Index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+const chefs = require('./data/chef.json');
+
+app.use(cors())
+app.get('/', (req, res) => {
+    res.send('chef-hunter is running')
+})
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
